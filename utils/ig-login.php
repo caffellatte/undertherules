8d801590cb6bd705b942bbd8384a337a4f9beace
caffellatte <m.g.lutsenko@gmail.com>
2017-04-27 01:18:37 +0300

    add nginx conf files, add cli arguments in ig-login.php

--- utils/ig-login.php
+++ utils/ig-login.php
@@ -1,8 +1,8 @@
 <?php
 # http://stackoverflow.com/questions/26003063
 
-$username = "*";
-$password = "*";
+$username = $argv[1];
+$password = $argv[2];
 $useragent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/50.0.2661.102 Chrome/50.0.2661.102 Safari/537.36";
 $cookie="../cookie.txt"; # $username.".txt";
 
