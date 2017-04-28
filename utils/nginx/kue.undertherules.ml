003c3e7029f0fdd13fd449a84c540be2af8ce9af
caffellatte <m.g.lutsenko@gmail.com>
2017-04-29 00:14:24 +0300

    Update nginx config:add kue. subdomain. Add npmignore.

--- utils/nginx/kue.undertherules.ml
+++ utils/nginx/kue.undertherules.ml
@@ -1,14 +1,14 @@
 # the IP(s) on which your node server is running. I chose port 8294.
-upstream app_undertherules {
-    server 127.0.0.1:8294;
+upstream app_kue_undertherules {
+    server 0.0.0.0:8816;
     keepalive 8;
 }
 
 # the nginx server instance
 server {
     listen 0.0.0.0:80;
-    server_name undertherules.ml  undertherules
-    access_log /var/log/nginx/undertherules.ml.log;
+    server_name kue.undertherules.ml;
+    access_log /var/log/nginx/kue.undertherules.ml.log;
 
     # pass the request to the node.js server with the correct headers
     # and much more can be added, see nginx config options
@@ -21,7 +21,7 @@ server {
       proxy_set_header Host $http_host;
       proxy_set_header X-NginX-Proxy true;
 
-      proxy_pass http://app_undertherules/;
+      proxy_pass http://app_kue_undertherules/;
       proxy_redirect off;
     }
  }
